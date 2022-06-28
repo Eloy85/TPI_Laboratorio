b4bf26ce616b3333e84265a259fcab0a7cdb7501
pipy24 <abigailellena24@gmail.com>
2022-06-28 14:52:38 -0300

    a

--- Python.py
+++ Python.py
@@ -51,22 +51,23 @@ if ele_menu==1:
 	os.system("clear")
 	while True:
 		while True:
-			a=random.randint(0,100)
-			if a%2==0:
-				print (a, "                                                           Escribe 100 para salir")
-				break
+			A=random.randint(0,99)
+			if A%2==0:
+			        print ("Escribe 100 para salir")
+			        print (A)
+			        break
 		while True:
-			b=random.randint(0,100)
-			if b%2!=0:
-				print(b)
+			B=random.randint(0,99)
+			if B%2!=0:
+				print(B)
 				break
 		eleccion=int(input("¿cual es el numero par? >> "))
-		if eleccion==a:
-			print("es par")
+		if eleccion==A:
+			print("correcto")
 			input("Presiona enter para continuar")
 			os.system("clear")
-		elif eleccion==b:
-			print("es impar")
+		elif eleccion==B:
+			print(" no coincide con la pregunta pero es impar")
 			input("Presiona enter para continuar")
 			os.system("clear")
 		elif eleccion==100:
@@ -77,14 +78,12 @@ if ele_menu==1:
 			input("Presiona enter para continuar")
 			os.system("clear")
 elif ele_menu==2:
-	print("NUMEROS PAR:")
+	print("REGLAS DEL JUEGO")
 	print("un numero par lo podemos identificar ya que si lo dividimos por 2 y su resto da 0")
 	print("un numero par tambien lo podemos identificar cuando termina en: 0, 2, 4, 6 u 8") 
-	print("NUMEROS IMPAR:")
 	print("un numero impar lo podemos identificar ya que si lo dividimos por 2 y su resto es diferente a 0")
 	print("un numero impar tambien lo podemos identificar cuando termina en : 1, 3, 5, 7, 9")
 	
 else:
-	print("adios")
 	os.system("exit")
 
