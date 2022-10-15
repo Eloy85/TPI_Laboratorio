edbe5227f06684a20cb15a2b89e391e3310e7ea9
TotiManuel <manuel.mandaio@gmail.com>
2022-10-15 03:39:38 +0000

    Primer commit para guardar archivos explicativos a mis compañeros

---
+++ Python.py
@@ -0,0 +1,11 @@
+print("Hola Mundo")     ## Mostrar "Hola Mundo"
+Entrada=input()         ## Leer Entrada
+print(Entrada)          ## Mostrar Entrada
+
+if Entrada =="1":       ## Si Entrada == "1" Entonces
+    print("Es un uno")  ## Mostrar "Es un uno"
+elif Entrada =="2":     ## Sino si Entrada == "2" Entonces
+    print("Es un dos")  ## Mostrar "Es un dos"
+else:                   ## Sino
+    print("Error")      ## Mostrar "Error"
+
