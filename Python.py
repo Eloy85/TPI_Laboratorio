daa825bdb3d69718131eb762e740820b2d14c680
TotiManuel <65439298+TotiManuel@users.noreply.github.com>
2022-10-16 22:26:02 -0300

    Se agrega el menu de los juegos

--- Python.py
+++ Python.py
@@ -1,11 +1,38 @@
-print("Hola Mundo")     ## Mostrar "Hola Mundo"
-Entrada=input()         ## Leer Entrada
-print(Entrada)          ## Mostrar Entrada
+##print("Hola Mundo")     ## Mostrar "Hola Mundo"
+##Entrada=input()         ## Leer Entrada
+##print(Entrada)          ## Mostrar Entrada
+##if Entrada =="1":       ## Si Entrada == "1" Entonces
+##    print("Es un uno")  ## Mostrar "Es un uno"
+##elif Entrada =="2":     ## Sino si Entrada == "2" Entonces
+##    print("Es un dos")  ## Mostrar "Es un dos"
+##else:                   ## Sino
+##    print("Error")      ## Mostrar "Error"
 
-if Entrada =="1":       ## Si Entrada == "1" Entonces
-    print("Es un uno")  ## Mostrar "Es un uno"
-elif Entrada =="2":     ## Sino si Entrada == "2" Entonces
-    print("Es un dos")  ## Mostrar "Es un dos"
-else:                   ## Sino
-    print("Error")      ## Mostrar "Error"
-
+while True:
+    print("")
+    print("                                 Bienvenidos a JAME")
+    print("")
+    print("1) Maribel")
+    print("2) Abigail")
+    print("3) Julian")
+    print("4) Eloy")
+    print("5) Indefinido")
+    print("6) Contacto")
+    print("7) Salir")
+    eleccion_menu=int(input())
+    if eleccion_menu==1:
+        #Juego de Maribel
+        print("a")
+    elif eleccion_menu==2:
+        #Juego de Abigail
+        print("a")
+    elif eleccion_menu==3:
+        #Juego de Julian
+        print("a")
+    elif eleccion_menu==4:
+        #Juego de Eloy
+        print("a")
+    elif eleccion_menu==7:
+        break
+    else:
+        print("Selecciona una Opcion correcta")
