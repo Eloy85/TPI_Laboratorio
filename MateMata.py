1aa53eb0750f23e7171f65368a6bd1ff1fccad3b
Eloy85 <eloyfmolina@gmail.com>
2022-11-24 16:23:48 -0300

    codigo juego eloy y menu principal listos

---
+++ MateMata.py
@@ -0,0 +1,145 @@
+import os
+import random
+
+def valorNum(num, dificultad):
+    if dificultad == 1:
+        num = random.randint(1, 9)
+    elif dificultad == 2:
+        num = random.randint(1, 99)
+    elif dificultad == 3:
+        num = random.randint(1, 999)
+    return num
+
+def puntos(dificultad, punto):
+    if dificultad == 1:
+        punto += 10
+        print("¡Correcto! Sumás 10 puntos.")
+    elif dificultad == 2:
+        punto += 20
+        print("¡Correcto! Sumás 20 puntos.")
+    elif dificultad == 3:
+        punto += 30
+        print("¡Correcto! Sumás 30 puntos.")
+    return punto
+
+salir = str()
+a = 0
+b = 0
+puntaje = 0
+
+tablaPosiciones = []
+
+for j in range(11):
+    tablaPosiciones.append([0] * 2)
+
+tablaPosiciones[0] = "Roxana", 3000
+tablaPosiciones[1] = "Francisco", 1500
+tablaPosiciones[2] = "Roberto", 1000
+tablaPosiciones[3] = "Elena", 500
+tablaPosiciones[4] = "Sandra", 300
+tablaPosiciones[5] = "Jorge", 250
+tablaPosiciones[6] = "Ignacio", 100
+tablaPosiciones[7] = "Zulema", 50
+tablaPosiciones[8] = "Gaston", 20
+tablaPosiciones[9] = "Pedro", 10
+
+
+while salir != "n":
+    print("****************************************************\n"
+          "¡Bienvenido a MateMata! Elige una categoría")
+    print("1 - SUMA \n2 - RESTA \n3 - MULTIPLICACION \n4 - DIVISION")
+    tipoOperacion = int(input("Ingresa la opción elegida (1 - 4): "))
+    while tipoOperacion > 4 or tipoOperacion < 1:
+        tipoOperacion = int(input("Opción incorrecta. Ingresa la opción elegida (1 - 4): "))
+    print("****************************************************\n"
+          "Seleccione el nivel de dificultad (1 - 3)")
+    print("1 - Fácil \n2 - Medio \n3 - Difícil")
+    nivel = int(input())
+    while nivel > 3 or nivel < 1:
+        nivel = int(input("Opción incorrecta. Seleccione el nivel de dificultad (1 - 3): "))
+
+    if tipoOperacion == 1:  # suma
+        i = 0
+        for i in range(10):
+            a = valorNum(a, nivel)
+            b = valorNum(b, nivel)
+            print("*** Pregunta", i+1, "de 10 ***")
+            print(a, "+", b, "=")
+            resultado = int(input())
+            if resultado == a + b:
+                puntaje = puntos(nivel, puntaje)
+            else:
+                print("Incorrecto. La respuesta correcta es", a + b)
+        print("Fin de la ronda. Tu puntaje total es", puntaje)
+    elif tipoOperacion == 2:  # resta
+        i = 0
+        for i in range(10):
+            a = valorNum(a, nivel)
+            b = valorNum(b, nivel)
+            while a < b:
+                a = valorNum(a, nivel)
+                b = valorNum(b, nivel)
+            print("*** Pregunta", i+1, "de 10 ***")
+            print(a, "-", b, "=")
+            resultado = int(input())
+            if resultado == a - b:
+                puntaje = puntos(nivel, puntaje)
+            else:
+                print("Incorrecto. La respuesta correcta es", a - b)
+        print("Fin de la ronda. Tu puntaje total es", puntaje)
+    elif tipoOperacion == 3:  # multiplicación
+        i = 0
+        for i in range(10):
+            a = valorNum(a, nivel)
+            b = valorNum(b, nivel)
+            print("*** Pregunta", i+1, "de 10 ***")
+            print(a, "x", b, "=")
+            resultado = int(input())
+            if resultado == a * b:
+                puntaje = puntos(nivel, puntaje)
+            else:
+                print("Incorrecto. La respuesta correcta es", a * b)
+        print("Fin de la ronda. Tu puntaje total es", puntaje)
+    elif tipoOperacion == 4:  # división
+        i = 0
+        for i in range(10):
+            a = valorNum(a, nivel)
+            b = valorNum(b, nivel)
+            while a % b != 0:
+                a = valorNum(a, nivel)
+                b = valorNum(b, nivel)
+            print("*** Pregunta", i+1, "de 10 ***")
+            print(a, "/", b, "=")
+            resultado = int(input())
+            if resultado == a / b:
+                puntaje = puntos(nivel, puntaje)
+            else:
+                print("Incorrecto. La respuesta correcta es", a / b)
+        print("Fin de la ronda. Tu puntaje total es", puntaje)
+
+    salir = str(input("¿Desea volver a jugar? S/N): "))
+    while salir != "s" and salir != "n":
+        salir = str(input("¿Desea volver a jugar? S/N): "))
+    print("\n")
+
+nombre = input("Ingresa tu nombre: ")
+
+puesto = 0
+for i in range(10):
+    if puntaje < tablaPosiciones[i][1]:
+        puesto = i + 1
+
+if puntaje > tablaPosiciones[puesto][1]:
+    for i in range(10, puesto, -1):
+        tablaPosiciones[i] = tablaPosiciones[i - 1]
+
+tablaPosiciones[puesto] = nombre, puntaje
+
+print("\n")
+print("****************************************************\n"
+      "TABLA DE POSICIONES")
+for i in range(10):
+    print(tablaPosiciones[i])
+print("****************************************************")
+
+os.system("python Menu.py")
--- Menu.py
+++ Menu.py
@@ -1,80 +1,41 @@
 import os
 
-def print_menu():
-    os.system("clear")
-    print("<><><><><><><><><><><><><><><><><><><><>")
-    print("<>                                    <>")
-    print("<>    Bienvenido a JAME               <>")           
-    print("<>    Que queres Aprender Jugando?    <>")
-    print("<>    1) Matematicas                  <>")
-    print("<>    2) Ingles                       <>")
-    print("<>    3) Programacion                 <>")
-    print("<>    4)                              <>")
-    print("<>    x) Salir                        <>")
-    print("<>                                    <>")
-    print("<><><><><><><><><><><><><><><><><><><><>")
-    print("")
+print("<><><><><><><><><><><><><><><><><><><><>\n"
+      "<>                                    <>\n"
+      "<>    Bienvenido/a!                   <>\n"
+      "<>    ¿Qué quieres aprender jugando?  <>\n"
+      "<>    1) MateMata                     <>\n"
+      "<>    2) TalesCapi                    <>\n"
+      "<>    x) Salir                        <>\n"
+      "<>                                    <>\n"
+      "<><><><><><><><><><><><><><><><><><><><>\n"
+      "\n")
 
-def print_menu_error():
-    os.system("clear")
-    print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
-    print("<>                                                                      <>")
-    print("<>    Bienvenido a JAME                                                 <>")
-    print("<>    Que queres Aprender Jugando?                                      <>")
-    print("<>    1) Matematicas                                                    <>")
-    print("<>    2) Ingles                                                         <>")
-    print("<>    3) Programacion                                                   <>")
-    print("<>    4)                                                                <>")
-    print("<>    x) Salir                                                          <>")
-    print("<>                                                                      <>")
-    print("<>    Por favor coloca una opcion valida. Ej: Para Ingles seria: 2      <>")
-    print("<>                                                                      <>")
-    print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
-
-
-def juego_matematicas():
-    print_menu()
-    print("1")
-    programa()
-
-def juego_ingles():
-    print_menu()
-    print("2")
-    programa()
-
-def juego_programacion():
-    os.system("clear")
-    print("")
-    print("Excelente!! Elegiste Programacion!!")
-    print("")
-    print("")
-
-def juego_cuatro():
-    print_menu()
-    print("4")
-    programa()
-
-def funcion_salir():
+opcion = input("Elige: ")
+if opcion == "1":
+    os.system("python MateMata.py")
+elif opcion == "2":
+    os.system("python TalesCapi.py")
+elif opcion == "x":
     os.system("exit")
 
-def default1():
-    print_menu_error()
-    programa()
-
-def programa():
-    Opcion=input("Elige: ")
-    if Opcion=="1":
-        juego_matematicas()
-    elif Opcion=="2":
-        juego_ingles()
-    elif Opcion=="3":
-        juego_programacion()
-    elif Opcion=="4":
-        juego_cuatro()
-    elif Opcion=="x":
-        funcion_salir()
-    else:
-        default1()
-
-print_menu()
-programa()
+while opcion != "1" and opcion != "2" and opcion != "x":
+    print("<><><><><><><><><><><><><><><><><><><><>\n"
+          "<>                                    <>\n"
+          "<>    Bienvenido/a!                   <>\n"
+          "<>    ¿Qué quieres aprender jugando?  <>\n"
+          "<>    1) MateMata                     <>\n"
+          "<>    2) TalesCapi                    <>\n"
+          "<>    x) Salir                        <>\n"
+          "<>                                    <>\n"
+          "<> Por favor coloca una opción válida.<>\n"
+          "<> Ej.: Para TalesCapi ingresa: 2     <>\n"
+          "<><><><><><><><><><><><><><><><><><><><>\n"
+          "\n")
+    opcion = input("Elige: ")
+    if opcion == "1":
+        os.system("python MateMata.py")
+    elif opcion == "2":
+        os.system("python TalesCapi.py")
+    elif opcion == "x":
+        os.system("exit")
