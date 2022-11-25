3acd4a4ed57f1df0c20718a4fd0db0b988e204dd
Eloy85 <eloyfmolina@gmail.com>
2022-11-25 10:06:54 -0300

    ultima version analisis y codigo

--- MateMata.py
+++ MateMata.py
@@ -48,15 +48,27 @@ while salir != "n":
     print("****************************************************\n"
           "¡Bienvenido a MateMata! Elige una categoría")
     print("1 - SUMA \n2 - RESTA \n3 - MULTIPLICACION \n4 - DIVISION")
-    tipoOperacion = int(input("Ingresa la opción elegida (1 - 4): "))
-    while tipoOperacion > 4 or tipoOperacion < 1:
-        tipoOperacion = int(input("Opción incorrecta. Ingresa la opción elegida (1 - 4): "))
+    tipoOperacion = 0
+    while tipoOperacion == 0:
+        try:
+            tipoOperacion = int(input("Ingresa la opción elegida (1 - 4): "))
+        except ValueError:
+            print("Por favor ingresa un número.")
+        else:
+            while tipoOperacion > 4 or tipoOperacion < 1:
+                tipoOperacion = int(input("Opción incorrecta. Ingresa la opción elegida (1 - 4): "))
     print("****************************************************\n"
           "Seleccione el nivel de dificultad (1 - 3)")
     print("1 - Fácil \n2 - Medio \n3 - Difícil")
-    nivel = int(input())
-    while nivel > 3 or nivel < 1:
-        nivel = int(input("Opción incorrecta. Seleccione el nivel de dificultad (1 - 3): "))
+    nivel = 0
+    while nivel == 0:
+        try:
+            nivel = int(input())
+        except ValueError:
+            print("Por favor ingresa un número.")
+        else:
+            while nivel > 3 or nivel < 1:
+                nivel = int(input("Opción incorrecta. Seleccione el nivel de dificultad (1 - 3): "))
 
     if tipoOperacion == 1:  # suma
         i = 0
@@ -65,11 +77,18 @@ while salir != "n":
             b = valorNum(b, nivel)
             print("*** Pregunta", i+1, "de 10 ***")
             print(a, "+", b, "=")
-            resultado = int(input())
-            if resultado == a + b:
-                puntaje = puntos(nivel, puntaje)
-            else:
-                print("Incorrecto. La respuesta correcta es", a + b)
+            resultado = int()
+            while resultado != a+b:
+                try:
+                    resultado = int(input())
+                except ValueError:
+                    print("Por favor ingresa un número.")
+                else:
+                    if resultado == a + b:
+                        puntaje = puntos(nivel, puntaje)
+                    else:
+                        print("Incorrecto. La respuesta correcta es", a + b)
+                        break
         print("Fin de la ronda. Tu puntaje total es", puntaje)
     elif tipoOperacion == 2:  # resta
         i = 0
@@ -81,11 +100,18 @@ while salir != "n":
                 b = valorNum(b, nivel)
             print("*** Pregunta", i+1, "de 10 ***")
             print(a, "-", b, "=")
-            resultado = int(input())
-            if resultado == a - b:
-                puntaje = puntos(nivel, puntaje)
-            else:
-                print("Incorrecto. La respuesta correcta es", a - b)
+            resultado = int()
+            while resultado != a - b:
+                try:
+                    resultado = int(input())
+                except ValueError:
+                    print("Por favor ingresa un número.")
+                else:
+                    if resultado == a - b:
+                        puntaje = puntos(nivel, puntaje)
+                    else:
+                        print("Incorrecto. La respuesta correcta es", a - b)
+                        break
         print("Fin de la ronda. Tu puntaje total es", puntaje)
     elif tipoOperacion == 3:  # multiplicación
         i = 0
@@ -94,11 +120,18 @@ while salir != "n":
             b = valorNum(b, nivel)
             print("*** Pregunta", i+1, "de 10 ***")
             print(a, "x", b, "=")
-            resultado = int(input())
-            if resultado == a * b:
-                puntaje = puntos(nivel, puntaje)
-            else:
-                print("Incorrecto. La respuesta correcta es", a * b)
+            resultado = int()
+            while resultado != a * b:
+                try:
+                    resultado = int(input())
+                except ValueError:
+                    print("Por favor ingresa un número.")
+                else:
+                    if resultado == a * b:
+                        puntaje = puntos(nivel, puntaje)
+                    else:
+                        print("Incorrecto. La respuesta correcta es", a * b)
+                        break
         print("Fin de la ronda. Tu puntaje total es", puntaje)
     elif tipoOperacion == 4:  # división
         i = 0
@@ -110,11 +143,18 @@ while salir != "n":
                 b = valorNum(b, nivel)
             print("*** Pregunta", i+1, "de 10 ***")
             print(a, "/", b, "=")
-            resultado = int(input())
-            if resultado == a / b:
-                puntaje = puntos(nivel, puntaje)
-            else:
-                print("Incorrecto. La respuesta correcta es", a / b)
+            resultado = int()
+            while resultado != a / b:
+                try:
+                    resultado = int(input())
+                except ValueError:
+                    print("Por favor ingresa un número.")
+                else:
+                    if resultado == a / b:
+                        puntaje = puntos(nivel, puntaje)
+                    else:
+                        print("Incorrecto. La respuesta correcta es", a // b)
+                        break
         print("Fin de la ronda. Tu puntaje total es", puntaje)
 
     salir = str(input("¿Desea volver a jugar? S/N): "))
